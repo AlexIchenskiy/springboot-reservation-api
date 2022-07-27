e2f54d246c9a826e9f595a42aead88fe06cf4108
Alex Ichenskiy <aichenskiy@gmail.com>
2022-07-27 13:14:44 +0200

    feat: add db initialization

--- src/main/java/com/agency04/devcademy/ApartmentReservationSystemApplication.java
+++ src/main/java/com/agency04/devcademy/ApartmentReservationSystemApplication.java
@@ -1,8 +1,12 @@
 package com.agency04.devcademy;
 
+import com.agency04.devcademy.model.Accommodation;
+import com.agency04.devcademy.repository.AccommodationRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.agency04.devcademy")
@@ -10,6 +14,14 @@ public class ApartmentReservationSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApartmentReservationSystemApplication.class, args);
+	}
+
+	@PostConstruct
+	void initDB(AccommodationRepository accommodationRepository) {
+		Accommodation accommodation1 = new Accommodation();
+		Accommodation accommodation2 = new Accommodation();
+		System.out.println("Preloading " + accommodationRepository.save(accommodation1));
+		System.out.println("Preloading " + accommodationRepository.save(accommodation2));
 	}
 
 }
