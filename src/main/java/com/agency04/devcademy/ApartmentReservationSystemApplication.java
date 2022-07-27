631abe72d38e9d08656f0755269899e568dbc5a5
Alex Ichenskiy <aichenskiy@gmail.com>
2022-07-27 10:18:12 +0200

    refactor: renamed main spring class

--- src/main/java/com/agency04/devcademy/ApartmentReservationSystemApplication.java
+++ src/main/java/com/agency04/devcademy/ApartmentReservationSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootProjectTemplateApplication {
+public class ApartmentReservationSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootProjectTemplateApplication.class, args);
+		SpringApplication.run(ApartmentReservationSystemApplication.class, args);
 	}
 
 }
