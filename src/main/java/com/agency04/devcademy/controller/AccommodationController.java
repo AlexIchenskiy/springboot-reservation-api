45b9fb19e879c3805327136c9a3400ebae3caabd
Alex Ichenskiy <aichenskiy@gmail.com>
2022-07-30 13:41:06 +0200

    refactor: change api path

--- src/main/java/com/agency04/devcademy/controller/AccommodationController.java
+++ src/main/java/com/agency04/devcademy/controller/AccommodationController.java
@@ -12,7 +12,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/accommodations")
+@RequestMapping("/api/accommodation")
 public class AccommodationController {
 
     private final AccommodationServiceImpl accommodationServiceImpl;
