7ab25321748d94662b774d137a6a047f247496b4
Alex Ichenskiy <aichenskiy@gmail.com>
2022-07-30 14:14:33 +0200

    refactor: change exception handling

--- src/main/java/com/agency04/devcademy/controller/ExceptionController.java
+++ src/main/java/com/agency04/devcademy/controller/ExceptionController.java
@@ -1,19 +1,37 @@
 package com.agency04.devcademy.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler(Exception.class)
-    public final ModelAndView handleAllExceptions(Exception e) {
-        ModelAndView model = new ModelAndView("error/generic_error");
+    public final ResponseEntity handleAllExceptions(Exception e, WebRequest request) {
+        List<String> details = new ArrayList<>();
 
-        model.addObject("errMsg", "This is Exception.class");
+        details.add(e.getLocalizedMessage());
 
-        return model;
+        ErrorResponse error = new ErrorResponse("Internal server error", details);
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity handleAccommodationNotFoundException(Exception e, WebRequest request) {
+        List<String> details = new ArrayList<>();
+
+        details.add(e.getLocalizedMessage());
+
+        ErrorResponse error = new ErrorResponse("Accommodation id does not exist", details);
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
 }
