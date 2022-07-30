4ad81d23c188cf69096b946a0994f90f4f3cc69d
Alex Ichenskiy <aichenskiy@gmail.com>
2022-07-30 14:25:49 +0200

    feat: add preloaded data

--- src/main/java/com/agency04/devcademy/service/impl/InitializeServiceImplHR.java
+++ src/main/java/com/agency04/devcademy/service/impl/InitializeServiceImplHR.java
@@ -1,6 +1,7 @@
 package com.agency04.devcademy.service.impl;
 
 import com.agency04.devcademy.model.Accommodation;
+import com.agency04.devcademy.model.AccommodationType;
 import com.agency04.devcademy.service.AccommodationService;
 import com.agency04.devcademy.service.InitializeService;
 import org.springframework.context.annotation.Profile;
@@ -18,11 +19,17 @@ public class InitializeServiceImplHR implements InitializeService {
 
     @Override
     public void initDatabase() {
-        Accommodation accommodation1 = new Accommodation();
-        Accommodation accommodation2 = new Accommodation();
-        Accommodation accommodation3 = new Accommodation();
+        Accommodation accommodation1 = new Accommodation("Sobe u Dubrovniku", "Grad",
+                "Hrvatski kulturni dragulj", AccommodationType.ROOM,
+                5, 4,
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Montage_of_major_Dubrovnik_landmarks.jpg/420px-Montage_of_major_Dubrovnik_landmarks.jpg",
+                false, 450.0);
+        Accommodation accommodation2 = new Accommodation("Apartmani na Mljetu", "Otok",
+                "Najljepši i najšumovitiji otok Jadrana", AccommodationType.APARTMENT,
+                5, 5,
+                "http://visitdubrovnik.hr/wp-content/uploads/2018/09/shutterstock_1101003428-1024x761.jpg",
+                false, 250.0);
         System.out.println("Preducitavanje " + this.accommodationService.save(accommodation1));
         System.out.println("Preducitavanje " + this.accommodationService.save(accommodation2));
-        System.out.println("Preducitavanje " + this.accommodationService.save(accommodation3));
     }
 }
