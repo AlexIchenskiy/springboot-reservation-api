8e934e277ecc56df256553dbb10b67ee59f4bb75
Alex Ichenskiy <aichenskiy@gmail.com>
2022-08-02 12:18:18 +0200

    fix: add logic to different accommodation services

--- src/main/java/com/agency04/devcademy/service/impl/MobileHomeAccommodationService.java
+++ src/main/java/com/agency04/devcademy/service/impl/MobileHomeAccommodationService.java
@@ -1,11 +1,17 @@
 package com.agency04.devcademy.service.impl;
 
+import com.agency04.devcademy.model.AccommodationType;
 import com.agency04.devcademy.repository.AccommodationRepository;
 
 public class MobileHomeAccommodationService extends AccommodationServiceImpl {
 
     public MobileHomeAccommodationService(AccommodationRepository accommodationRepository) {
         super(accommodationRepository);
+    }
+
+    @Override
+    public AccommodationType getAccommodationType() {
+        return AccommodationType.MOBILE_HOME;
     }
 
 }
--- src/main/java/com/agency04/devcademy/service/impl/RoomAccommodationService.java
+++ src/main/java/com/agency04/devcademy/service/impl/RoomAccommodationService.java
@@ -1,11 +1,17 @@
 package com.agency04.devcademy.service.impl;
 
+import com.agency04.devcademy.model.AccommodationType;
 import com.agency04.devcademy.repository.AccommodationRepository;
 
 public class RoomAccommodationService extends AccommodationServiceImpl {
 
     public RoomAccommodationService(AccommodationRepository accommodationRepository) {
         super(accommodationRepository);
+    }
+
+    @Override
+    public AccommodationType getAccommodationType() {
+        return AccommodationType.ROOM;
     }
 
 }
