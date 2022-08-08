bc7492739a63e7808cc7ca4d49ea89c184a80bd3
Alex Ichenskiy <aichenskiy@gmail.com>
2022-08-08 16:22:29 +0200

    [SHA-11] fix: fixed test

--- src/test/java/com/agency04/devcademy/service/impl/AccommodationServiceImplTest.java
+++ src/test/java/com/agency04/devcademy/service/impl/AccommodationServiceImplTest.java
@@ -1,20 +1,29 @@
 package com.agency04.devcademy.service.impl;
 
+import com.agency04.devcademy.ApartmentReservationSystemApplication;
 import com.agency04.devcademy.model.Accommodation;
 import com.agency04.devcademy.repository.AccommodationRepository;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class AccommodationServiceImplTest {
-
-    AccommodationServiceImpl accommodationService;
 
     @Mock
     AccommodationRepository accommodationRepository;
@@ -22,21 +31,13 @@ class AccommodationServiceImplTest {
     @Before
     public void setUp() {
         MockitoAnnotations.openMocks(this);
-
-        accommodationService = new AccommodationServiceImpl(accommodationRepository);
     }
 
     @Test
     public void findAll() {
-        Accommodation accommodation = new Accommodation();
+        List<Accommodation> accommodations = accommodationRepository.findAll();
 
-        accommodationService.save(accommodation);
-
-        List<Accommodation> accommodations = accommodationService.findAll();
-
-        when(accommodationRepository.findAll()).thenReturn(accommodations);
-
-        assertEquals(accommodations.size(), 1);
+        Assertions.assertEquals(0, accommodations.size());
 
         verify(accommodationRepository, times(1)).findAll();
     }
