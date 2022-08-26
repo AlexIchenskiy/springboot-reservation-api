f6f3595e05534bf77a078bc0663732a81910c761
Alex Ichenskiy <aichenskiy@gmail.com>
2022-08-26 12:50:03 +0200

    fix: fixed given authorities

--- src/main/java/com/agency04/devcademy/service/impl/LoginServiceImpl.java
+++ src/main/java/com/agency04/devcademy/service/impl/LoginServiceImpl.java
@@ -9,17 +9,23 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LoginServiceImpl implements LoginService {
 
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UsersServiceImpl usersService;
+
     @Override
     public void authenticate(String email, String password) {
         try {
             UsernamePasswordAuthenticationToken authentication =
-                    new UsernamePasswordAuthenticationToken(email, password);
+                    new UsernamePasswordAuthenticationToken(email, password,
+                            usersService.findByEmail(email).getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
             authenticationManager.authenticate(authentication);
         } catch (AuthenticationException e) {
